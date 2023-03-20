6db4d661ff75e5bbd6354b1a15ec5ad51de1b2ed
Ada Bilinska <55477233+555ada555@users.noreply.github.com>
2023-03-20 21:44:10 +0200

    Add files via upload

---
+++ LW3/LW3/Program.cs
@@ -0,0 +1,66 @@
+﻿using LW3.Observer;
+using LW3.Payment;
+using LW3.Сart;
+using System;
+using System.Reflection.Metadata;
+using System.Xml.Linq;
+
+namespace StrategyDesignPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /////////////////////////////////////////
+            //Strategy Pattern
+            Console.WriteLine("Будь ласка, виберіть тип платежу: кредитна картка або готівка");
+            string PaymentType = Console.ReadLine();
+            Console.WriteLine("Тип платежу : " + PaymentType);
+            double Amount = 1000;
+            Console.WriteLine("Сума : " + Amount);
+
+            PaymentS context = new PaymentS();
+
+            if ("CreditCard".Equals(PaymentType, StringComparison.InvariantCultureIgnoreCase))
+            {
+                context.SetPaymentStrategy(new CreditCard());
+            }
+            else if ("Cash".Equals(PaymentType, StringComparison.InvariantCultureIgnoreCase))
+            {
+                context.SetPaymentStrategy(new Cash());
+            }
+            context.Pay(Amount);
+
+
+
+            /////////////////////////////////////////
+            //Command Pattern
+            CartProduct document = new CartProduct();
+            ICommand addCommand = new AddCommand(document);
+            ICommand subtractCommand = new SubtractCommand(document);
+            ICommand deleteCommand = new DeleteCommand(document);
+            CartProductOption menu = new CartProductOption(addCommand, subtractCommand, deleteCommand);
+            menu.clickAdd();
+            menu.clickSubtract();
+            menu.clickDelete();
+
+
+            ///////////////////////////////////////////
+            // Observer  Pattern
+            Product book = new Product("Book001", 10000, "Немає в наявності");
+            //Буде створено користувача ANNET, а об’єкт user1 буде зареєстровано для суб’єкта
+            Observer user1 = new Observer("ANNET", book);
+
+            Observer user2 = new Observer("LOLI", book);
+            Observer user3 = new Observer("Sonya555", book);
+
+            Console.WriteLine("Поточний стан : " + book.getAvailability());
+            Console.WriteLine();
+            // Зараз товар доступний
+            book.setAvailability("Доступно");
+            Console.Read();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LW3/LW3/Сart/SubtractCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW3.Сart
+{
+    public class SubtractCommand : ICommand
+    {
+        private CartProduct product;
+        public SubtractCommand(CartProduct prod)
+        {
+            product = prod;
+        }
+        public void Execute()
+        {
+            product.Subtract();
+        }
+    }
+}
